1d379bf7427159feb2e0cef11a1a35f1f43236dd
Sachin Narang <sachinnarang777@gmail.com>
2021-08-13 11:49:12 +0530

    first commit

---
+++ src/test/java/com/vwit/eurekaservice/EurekaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vwit.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
